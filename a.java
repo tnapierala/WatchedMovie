6979c60240123b34f6ece0d60b57d776e50d3483
Tomek <tnapierala@edu.cdv.pl>
2021-11-30 19:02:41 +0100

    drugi

---
+++ a.java
@@ -0,0 +1,15 @@
+try {
+    ResultSet result = wykonajZapytanie( sqlText );
+} catch (SQLException e) {
+    e.printStackTrace();
+}
+"SELECT * FROM pracownicy"
+        try {
+; 
+            ResultSet result;
+            result = wykonajZapytanie( selectQuery:  ));
+            System.out.println(result);
+        } catch (SQLException e) {
+            e.printStackTrace(); 
+        }
+        //
